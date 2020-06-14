20ce137ab9fd164d7293f1ad3b1ea682f09e90e4
Gteixeirinha7 <gabrielteixeir137@gmail.com>
2020-06-13 22:01:54 -0300

    Car CRU_

--- src/main/java/servlet/ServletCar.java
+++ src/main/java/servlet/ServletCar.java
@@ -26,6 +26,37 @@ import org.json.simple.JSONObject;
 public class ServletCar extends APIHandler {
 
     @Override
+    public JSONObject executeGET(JSONObject inputData) throws AppException, SQLException {
+        JSONObject returnData = new JSONObject();
+        JSONObject returnInternalData = new JSONObject();
+        String sfid = null;
+        if (inputData.containsKey("ExternalId")) {
+            System.out.println("Read Call");
+            sfid = inputData.get("ExternalId").toString();
+            ResultSet rs = this.executeQuery("SELECT sfid, externalid__c, Armored__c, Color__c, Exchange__c, Fuel__c, Price__c, UsedCar__c, Year__c, Name " + 
+                    " FROM salesforce.Car__C"
+                    + " WHERE externalid__c = '" + sfid + "' AND isdeleted = false");
+            if (!rs.next()) {
+                throw new AppException("Fail to load Car, Car doesn't exists", "APICar.executePOST");
+            }
+            returnInternalData.put("SalesforceId", rs.getString("sfid"));
+            returnInternalData.put("ExternalId", rs.getString("externalid__c"));
+            returnInternalData.put("Armored", rs.getBoolean("Armored__c"));
+            returnInternalData.put("Color", rs.getString("Color__c"));
+            returnInternalData.put("Exchange", rs.getString("Exchange__c"));
+            returnInternalData.put("Fuel", rs.getString("Fuel__c"));
+            returnInternalData.put("Price", rs.getDouble("Price__c"));
+            returnInternalData.put("UsedCar", rs.getBoolean("UsedCar__c"));
+            returnInternalData.put("Year", rs.getInt("Year__c"));
+            returnInternalData.put("Name", rs.getString("Name"));
+        }else{
+            throw new AppException("Fail to load Car, specify a 'ExternalId'", "APICar.executeGET");
+        }
+        returnData.put("statusCode", "200");
+        returnData.put("objectData", returnInternalData);
+        return returnData;
+    }
+    @Override
     public JSONObject executePOST(JSONObject inputData) throws AppException, SQLException {
         JSONObject returnData = new JSONObject();
         String sfid = null;
@@ -47,7 +78,7 @@ public class ServletCar extends APIHandler {
                     " UsedCar__c = " + AppUtils.boolVal(inputData.get("UsedCar").toString()) + "," + 
                     " Year__c = " + Integer.valueOf(inputData.get("Year").toString()) + 
                      " WHERE externalid__c = '" + sfid.toString() + "'");
-            returnData.put("internalCode", rs.getString("externalid__c"));
+            returnData.put("ExternalId", rs.getString("externalid__c"));
         
         }else{
             System.out.println("Insert Call");
@@ -70,7 +101,7 @@ public class ServletCar extends APIHandler {
             ResultSet rs = this.executeQuery(
                     "SELECT sfid FROM salesforce.Car__C WHERE externalid__c = '" + externalId + "' AND isdeleted = false");
                     
-            returnData.put("internalCode", externalId);
+            returnData.put("ExternalId", externalId);
         }
         returnData.put("statusCode", "200");
         return returnData;
